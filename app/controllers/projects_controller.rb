e767693f73feb7aa748f389234e85732cb4173e8
github username <github email address>
2019-11-04 22:28:40 -0600

    sockets to leave room

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,24 +1,25 @@
 class ProjectsController < ApplicationController
     def create
         @project = Project.create(project_params)
+        @@chat_room = ChatRoom.create(project_id: @project.id)
         @user = User.find(@project.novice_id)
-        render json: { user: @user.as_json(include: [:novice_projects, :expert_projects]) }
+        render json: @user
+        # { user: @user.as_json(include: [ {:novice_projects, :expert_projects }]) }
     end
 
     def show
         @project = Project.find(params[:id])
         render json: @project
-        #  { project: @project.as_json(include: [:materials, :tasks, :resources, :chat_room]) }
     end
 
     def update
         @project = Project.find(params[:id])
-
         if @project.expert
             @project.expert_id = nil
             @project.save
         else
-            @project.expert = User.find(id)
+            @project.expert = User.find(params[:expert_id])
+            @project.save
         end
         render json: @project
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :novice_projects, :expert_projects, :image
 
+  def novice_projects
+    return unless object.novice_projects
+    ActiveModelSerializers::SerializableResource.new(object.novice_projects,  each_serializer: ProjectSerializer)
+  end
+
   def image
     Rails.application.routes.url_helpers.rails_blob_path(object.image, only_path: true)
   end
