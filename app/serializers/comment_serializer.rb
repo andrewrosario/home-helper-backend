55d2c10b953b5364193fc48ccc745e972a70108a
github username <github email address>
2019-11-08 07:53:56 -0600

    fixed bug with remove expert

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :text, :created_at, :user_name
+  attributes :id, :text, :created_at, :user_name, :task_id, :material_id, :resource_id
 
   def user_name
     object.user.name
