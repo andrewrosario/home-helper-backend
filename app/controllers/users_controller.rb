54c0c24024dbe14e97d68a2e871c13f700fccfa0
github username <github email address>
2019-11-09 09:40:59 -0600

    render as json user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
     def index 
        @users = User.joins(:expert_ins).where('project_type_id = ?', params[:project_type_id].to_i)
-       render json: @users
+       render json: @users.as_json
     end
 
     def create
