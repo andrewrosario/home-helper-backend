77caed95e8851fc5ee80a092bc675a69b16fe8b7
github username <github email address>
2019-11-11 11:25:06 -0600

    testing patch update route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    skip_before_action :authorized, only: [:index, :update]
 
     def index
         projects = Project.all
